9fdd3f02df631a826dde1f37ccfb6fcb9c0cdc2a
Flavio Luna <flavio.luna@bittobyte.cl>
2021-02-18 11:58:46 -0300

    add list, select item, display item selected

--- tour-heroes/src/app/heroes/heroes.component.ts
+++ tour-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './heroes-mock';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +11,17 @@ export class HeroesComponent implements OnInit {
 
 
   hero : Hero = { id : 1 , name : 'spider man'};
+  heroes = HEROES;
+  selectedHeroe : Hero = { id:0, name:''};
 
   constructor() { }
 
   ngOnInit(): void {
+  }
 
+  onSelectHeroe(h : Hero){
+      this.selectedHeroe = h;
+      console.log(this.selectedHeroe);
   }
 
 }
